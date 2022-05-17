a1143de963f9e18c926e84456701ce9c84475eb5
Irina403 <92080770+Irina403@users.noreply.github.com>
2022-05-17 15:15:36 +0300

    start 17.05 15:15

--- assets/js/main.js
+++ assets/js/main.js
@@ -145,7 +145,23 @@ $('.js-tab-trigger').click(function(){
 		content.addClass('active');
 });
 
+var items = $(".list-wrapper .list-item");
+    var numItems = items.length;
+    var perPage = 4;
 
+    items.slice(perPage).hide();
+
+    $('#pagination-container').pagination({
+        items: numItems,
+        itemsOnPage: perPage,
+        prevText: "&laquo;",
+        nextText: "&raquo;",
+        onPageClick: function (pageNumber) {
+            var showFrom = perPage * (pageNumber - 1);
+            var showTo = showFrom + perPage;
+            items.hide().slice(showFrom, showTo).show();
+        }
+    });
 
 
 
